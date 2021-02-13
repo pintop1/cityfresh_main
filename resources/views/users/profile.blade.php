baa3f2fbda920ab528304f8703d57702689376c6
Pintop Technologies <info@pintoptechnologies.com>
2021-02-13 22:24:20 +0100

    Profile mobility
    Themobileversionofthesepagesarenotresponsive
    1. Addpersonalinfo 2. Addcard
    3. Addbankdetails

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -153,23 +153,23 @@
                                 @csrf
                                 @method('PUT')
                                 <div class="row">
-                                    <div class="form-group col-4">
+                                    <div class="form-group col-lg-4">
                                         <label>Passport</label>
                                         <input type="file" name="passport" class="form-control">
                                     </div>
-                                    <div class="form-group col-4">
+                                    <div class="form-group col-lg-4">
                                         <label>Name</label>
                                         <input type="text" name="name" class="form-control" value="{{ $user->name }}" required>
                                     </div>
-                                    <div class="form-group col-4">
+                                    <div class="form-group col-lg-4">
                                         <label>Phone</label>
                                         <input type="number" name="phone" class="form-control" value="{{ $user->phone }}" readonly>
                                     </div>
-                                    <div class="form-group col-4">
+                                    <div class="form-group col-lg-4">
                                         <label>Date of birth</label>
                                         <input type="date" name="dob" class="form-control" value="{{ $user->dob }}">
                                     </div>
-                                    <div class="form-group col-4">
+                                    <div class="form-group col-lg-4">
                                         <label>Address</label>
                                         <input type="text" name="address" class="form-control" value="{{ $user->address }}">
                                     </div>
@@ -278,7 +278,7 @@
                                     </tbody>
                                 </table>
                             </div>
-                            <div class="col-4">
+                            <div class="col-lg-4">
                                 <h4 class="mt-2 mb-4 header-title">Add New Bank</h4>
                                 <form action="{{ route('banks.store') }}" method="post">
                                     @csrf
