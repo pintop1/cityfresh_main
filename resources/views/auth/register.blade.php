742bae0e8d14e2ffe48b8e8fb85364c636a360cb
Pintop Technologies <info@pintoptechnologies.com>
2021-02-13 22:05:31 +0100

    ThefontonthereferralPopupistoobig.Pleasereduceittothenormal heading on the registration page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -87,7 +87,7 @@
     $(function(){
         $('.onAct').on("click", function (e) {
             Swal.fire({
-              text: 'Were you referred to us by an active user?<br> Please enter the user\'s referral code below.',
+              html: 'Were you referred to us by an active user?<br> Please enter the user\'s referral code below.',
               input: 'text',
               inputAttributes: {
                 autocapitalize: 'off'
